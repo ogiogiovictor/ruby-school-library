ca5c3c4fbb2e6f27c93e409fea9ac012d3a875c5
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-11-01 05:02:41 -0600

    fixed linters

--- methods/teacher.rb
+++ methods/teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true, specialization: 'Unknown')
-    super(id, name, age, parent_permission: parent_permission)
+  def initialize(age, name = 'Unknown', parent_permission: true, specialization: 'Unknown')
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
@@ -13,5 +13,5 @@ class Teacher < Person
   end
 end
 
-teacher = Teacher.new(1, 'John', 2, parent_permission: true, specialization: 'Doctor')
+teacher = Teacher.new(15, 'John', parent_permission: true, specialization: 'Doctor')
 p teacher.can_use_services?
