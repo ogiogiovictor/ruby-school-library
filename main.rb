a348df3982b4e244fcac08c57f925af6827192c7
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-11-07 14:24:23 -0600

    refactor code to use SOLID

--- main.rb
+++ main.rb
@@ -2,59 +2,29 @@ require_relative './app'
 
 def choose_options
   puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
+  @options = { 1 => 'List all books', 2 => 'List all people', 3 => 'Create a person', 4 => 'Create a book',
+               5 => 'Create a rental', 6 => 'List all rentals for a given person id', 7 => 'Exit' }
+  @options.each { |key, value| puts "#{key} - #{value}\n" }
 end
 
 # rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/MethodLength
 def main
   app = App.new
   puts 'Welcome to School Library App!'
   puts "\n"
-  choose_options
-  selected = gets.chomp
-  
-  app.list_books if selected.to_i ==1
-  app.list_all_persons if selected.to_i ==2
-  app.create_person if selected.to_i ==3
-  app.create_book if selected.to_i ==4
-  app.create_rental if selected.to_i ==5
-  app.list_all_rentals_for_person_id if selected.to_i ==6
-  puts 'Thank you for using this app!' if selected.to_i ==7
-  # loop do
-  #   choose_options
-  #   option = gets.chomp
 
-  #   case option
-  #   when '1'
-  #     app.list_books(option)
-  #   when '2'
-  #     app.list_all_persons(option)
-  #   when '3'
-  #     app.create_person
-  #   when '4'
-  #     app.create_book
-  #   when '5'
-  #     app.create_rental
-  #   when '6'
-  #     app.list_all_rentals_for_person_id
-  #   when '7'
-  #     puts 'Thank you for using this app!'
-  #     break
-  #   else
-  #     puts 'That is not a valid option'
-  #     sleep(1)
-  #   end
-  #   puts "\n"
-  # end
+  loop do
+    choose_options
+    selected = gets.chomp
+
+    app.list_books(selected) if selected.to_i == 1
+    app.list_all_persons(selected) if selected.to_i == 2
+    app.create_person if selected.to_i == 3
+    app.create_book if selected.to_i == 4
+    app.create_rental if selected.to_i == 5
+    app.list_all_rentals_for_person_id if selected.to_i == 6
+    puts 'Thank you for using this app!' if selected.to_i == 7
+  end
 end
 # rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/MethodLength
-
 main
