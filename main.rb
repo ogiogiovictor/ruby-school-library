0369b8ca0218131b99423bf7979299d09d80240d
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-11-07 14:00:43 -0600

    refactoring code

--- main.rb
+++ main.rb
@@ -2,13 +2,17 @@ require_relative './app'
 
 def choose_options
   puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
+  @options = {1=> "List all books", 2=> "List all people", 3=> "Create a person", 4=> "Create a book", 5=> "Create a rental", 6=> "List all rentals for a given person id", 7=> "Exit"}
+ 
+  @options.each { |key, value| puts "#{key} - #{value}" + "\n" }
+
+  # puts '1 - List all books'
+  # puts '2 - List all people'
+  # puts '3 - Create a person'
+  # puts '4 - Create a book'
+  # puts '5 - Create a rental'
+  # puts '6 - List all rentals for a given person id'
+  # puts '7 - Exit'
 end
 
 # rubocop:disable Metrics/CyclomaticComplexity
