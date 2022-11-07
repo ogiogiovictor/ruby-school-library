5f2a8cecee324bd8fd4febe7dcf1c529b3f21b12
Tadesse Jemal <tadessejemal63@gmail.com>
2022-11-07 23:07:10 +0300

    change case condition to if

--- main.rb
+++ main.rb
@@ -17,33 +17,42 @@ def main
   app = App.new
   puts 'Welcome to School Library App!'
   puts "\n"
+  choose_options
+  selected = gets.chomp
+  
+  app.list_books if selected.to_i ==1
+  app.list_all_persons if selected.to_i ==2
+  app.create_person if selected.to_i ==3
+  app.create_book if selected.to_i ==4
+  app.create_rental if selected.to_i ==5
+  app.list_all_rentals_for_person_id if selected.to_i ==6
+  puts 'Thank you for using this app!' if selected.to_i ==7
+  # loop do
+  #   choose_options
+  #   option = gets.chomp
 
-  loop do
-    choose_options
-    option = gets.chomp
-
-    case option
-    when '1'
-      app.list_books(option)
-    when '2'
-      app.list_all_persons(option)
-    when '3'
-      app.create_person
-    when '4'
-      app.create_book
-    when '5'
-      app.create_rental
-    when '6'
-      app.list_all_rentals_for_person_id
-    when '7'
-      puts 'Thank you for using this app!'
-      break
-    else
-      puts 'That is not a valid option'
-      sleep(1)
-    end
-    puts "\n"
-  end
+  #   case option
+  #   when '1'
+  #     app.list_books(option)
+  #   when '2'
+  #     app.list_all_persons(option)
+  #   when '3'
+  #     app.create_person
+  #   when '4'
+  #     app.create_book
+  #   when '5'
+  #     app.create_rental
+  #   when '6'
+  #     app.list_all_rentals_for_person_id
+  #   when '7'
+  #     puts 'Thank you for using this app!'
+  #     break
+  #   else
+  #     puts 'That is not a valid option'
+  #     sleep(1)
+  #   end
+  #   puts "\n"
+  # end
 end
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/MethodLength
