6424c74733d7f2b7d0322109efe202805637bc83
Tadesse Jemal <tadessejemal63@gmail.com>
2022-11-10 18:26:50 +0300

    fix lintrce error

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe Student do
-    context 'It should add a student' do
-      student = Student.new(28, 'Tadesse', 'new classroom', parent_permission: true)
-  
-      it 'student should be an instance of class Student' do
-        expect(student).to be_an_instance_of(Student)
-      end
-  
-      it 'expects method play hooky to return ¯\\(ツ)/¯' do
-        expect(student.play_hooky).to eql('¯\\(ツ)/¯')
-      end
-  
-      it 'creates a student with permission' do
-        expect(student.can_use_services?).to eq true
-      end
+  context 'It should add a student' do
+    student = Student.new(28, 'Tadesse', 'new classroom', parent_permission: true)
+
+    it 'student should be an instance of class Student' do
+      expect(student).to be_an_instance_of(Student)
     end
+
+    it 'expects method play hooky to return ¯\\(ツ)/¯' do
+      expect(student.play_hooky).to eql('¯\\(ツ)/¯')
+    end
+
+    it 'creates a student with permission' do
+      expect(student.can_use_services?).to eq true
+    end
+  end
 end
