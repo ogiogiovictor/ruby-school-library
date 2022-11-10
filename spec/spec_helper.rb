7cc62967b3ac93912bea7c8db3c11bd29a6903fc
Tadesse Jemal <tadessejemal63@gmail.com>
2022-11-10 18:18:21 +0300

    Add testing book, person and student

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require_relative '../associations/book'
+require_relative '../classes/person'
+require_relative '../associations/classroom'
+require_relative '../classes/student'
+require_relative '../associations/rental'
+require_relative '../classes/teacher'
